e11f4072db6e565d392fe19109ebd92a75b6a7c8
sunghyun <ccorro211@gmail.com>
2024-06-02 00:06:40 +0900

    로고메인푸터터css

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,15 @@
-export default function About(){
-    return (
+import Image from "next/image"
+import styles from "./about.module.css"
+
+const About = () => {
+    return (                
         <div>
-            AboutPage
+            <div className={styles.imgContainer}>
+                <Image src="/about.png" alt="" fill/>
+            </div>
+            
         </div>
     )
 }
+
+export default About
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,24 @@
-
-
+import styles from "./page.module.css"
+import Image from "next/image";
 export default function Home() {
   return (
-    <div>
-      Homepage
+    <div className={styles.container}>
+        <div className={styles.textcontainer}>
+          <h1 className={styles.title}>Create Thoughts Agency</h1>
+          <p className={styles.desc}>메인페이지의 텍스트콘테이너 p태그 연습중</p>
+          <div className={styles.buttons}> 
+            <button className={styles.button}>Lean More</button>
+            <button className={styles.button}>Contact</button>
+          </div>
+        
+          <div className={styles.brands}>
+            <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="/hero.gif" alt="" fill className={styles.heroImg}/>
+        </div>
+       
     </div>
   );
 }
